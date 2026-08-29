ffd9a33cd95754202379ec4cad55e4cb2fe96317
clet001 <nwaforclement7@gmail.com>
2026-08-29 13:03:33 +0100

    I worked on book models and repository.

---
+++ src/e_library_system/models/book.py
@@ -0,0 +1,15 @@
+from typing import Optional
+from uuid import UUID, uuid4
+
+import UUID
+from pydantic import BaseModel, Field
+
+
+class Book(BaseModel):
+    title: str = Field(..., min_length=3, max_length=50)
+    author: str
+    id: UUID = Field(default_factory=uuid4)
+    isbn: Optional[str] = None
+    category: str
+    total_copies: int
+    available_copies: int
