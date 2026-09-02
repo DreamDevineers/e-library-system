8f769368dfd53607283ee89c564ee44d87406dc5
olukayode-asemudara <theolukayodeasemudara@gmail.com>
2026-09-02 21:32:47 +0100

    about page works fine as expected...been able to fix the bug that wasn't making it render

--- src/e-library-frontend/js/app.js
+++ src/e-library-frontend/js/app.js
@@ -1,13 +1,30 @@
 import Header from "./components/header.js";
 import hero from "./components/hero.js";
 
-const App = () => `
-    ${Header()}
-    
+const App = () => {
 
-    <main>
-        ${hero()}
-    </main>
-`;
+    const path = window.location.pathname;
+
+    console.log("Current path:", path);
+
+    if (path.includes("about.html")) {
+        return `
+            ${Header()}
+
+            <main>
+                <h1>ABOUT PAGE</h1>
+                <p>about page</p>
+            </main>
+        `;
+    }
+
+    return `
+        ${Header()}
+
+        <main>
+            ${hero()}
+        </main>
+    `;
+};
 
 export default App;
